845f7b5c10d20cfa079c0f69ba68c404d60f89d5
Mahdi Tehrani <mahdi@frequencydesign.com>
2016-12-12 22:04:54 -0800

    Initial structure

---
+++ mealcalc.js
@@ -0,0 +1,4 @@
+var Dish = require('./models/Dish.js');
+var Dinner = require('./models/Dinner.js');
+var Diner = require('./models/Diner.js');
+var TaxAndTip = require ('./models/meal_tax.js');
---
+++ models/meal_tax.js
@@ -0,0 +1,1 @@
+console.log("meal_tax");
